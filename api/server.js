88de90005b698f74b6947ffc241887fde72287fa
laurensruiz <laurenruiz97@yahoo.com>
2022-05-09 12:56:22 -0400

    finished post

--- api/server.js
+++ api/server.js
@@ -1,12 +1,38 @@
 // BUILD YOUR SERVER HERE
 
 const express = require('express');
-const User = require('./users/model')
+const User = require('./users/model') //look at all the functions created
 
 const server = express()
 
+server.use(express.json()) // we need to teach express how to parse
+
+//post
+server.post('/api/users', (req, res) => {
+     //http post :9000/api/users name=foo bio=bar --verbose
+    const user = req.body;
+    if(!user.name || !user.bio){
+        res.status(400).json({
+            message: "Please provide name and bio for the user",
+        })
+    } else {
+        User.insert(user)
+        .then(newUser => {
+            //console.log(data)
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error creating user",
+                err: err.message,
+            })
+        })
+    }  
+})
+
+//get
 server.get('/api/users', (req, res) => {
-    //res.json('users')
+    //res.json('users') http get :9000/api/users 
     User.find()
     .then(users =>{
         //throw new Error('whhops!')
